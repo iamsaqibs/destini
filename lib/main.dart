7c3f5c5337d50a1dcfa13391e791854ef963ea70
Saqib S <msaqibsaeed4@gmail.com>
2019-08-17 16:29:54 +0500

    Basic App Skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
   MaterialApp(
-    home: Text('data'),
+    home: SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: Destini(),
+      ),
+    ),
   ),
 );
+
+
+class Destini extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StoryPage();
+  }
+}
+
+class StoryPage extends StatefulWidget {
+  @override
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
